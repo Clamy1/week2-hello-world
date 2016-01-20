6264fbc26a0dfdf7af8ae67877d334a9d9e00e85
Christopher Lamy <christopher@Christophers-MacBook-Pro.local>
2016-01-20 18:16:59 -0500

    Lang.
    Learning how to say Hello

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,25 @@
-# replace the contents of this comment with your full name
+# Christopher Lamy
 
-# write a program that:
-# 1. greets the user in English
-# 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
-# 3. displays the greeting in the chosen language
-# 4. exits
+print('Hello. What language would you like to learn how to say "Hello" in?')# Question
+
+#Options
+print('A: Creole.')
+print('B: Swahili.')
+print('C: Swedish ')
+
+#Input
+option = input()
+
+#Choice of language user would like to learn.
+if option == 'A':
+    print('Alo')
+elif option == 'B':
+    print('Hujambo!')
+elif option == 'C':
+    print('Hej')
+#The end
+print('Good Bye!')
+
 
 # make sure that your code contains comments explaining your logic!
 
